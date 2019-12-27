7b345e466859cef149dd3bda69743777e0e92f85
avinash sahu <avi.sahu2009@gmail.com>
2019-12-27 11:30:52 +0530

    hos-service-discovery

---
+++ src/test/java/com/hos/hosservicediscovery/HosServiceDiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hos.hosservicediscovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HosServiceDiscoveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
